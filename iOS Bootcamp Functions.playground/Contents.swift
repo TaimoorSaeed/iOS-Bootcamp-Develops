7912f9aed2c3fe47570b2779b4ef8b219358f44a
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-08 19:49:41 +0500

    day1-exercise

---
+++ iOS Bootcamp Functions.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+func add (first: Double, second: Double) -> Double{
+    let result = first + second
+    return result
+}
+
+add(first: 5 , second: 8)
+
+
+func subtract (first: Double , second : Double) -> Double {
+    let result = second - first
+    return result
+}
+
+subtract(first: 5, second: 10)
+add(first: 5 , second : 10)
+
+
+func multiply(first : Float , second : Float) -> Float{
+    let result = first * second
+    return result
+}
+
+multiply(first: 3, second: 2)
+
+func divide(first: Double , second: Double) -> Double
+{
+    let result = first / second
+    return result
+}
+
+divide(first: 5, second : 2)
+
+
+
+
+
+
+
+
+
+
+
---
+++ iOS Bootcamp String.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var firstVar = 15
+var secondVar = 5
+ 
+var add = firstVar + secondVar
+
+var mul = firstVar * 3
+
+var sub = firstVar - secondVar
+
+var div = firstVar / secondVar
+
+var sample : Double
+
+var evar : Int 	= 423
+
+var hello : String = "Hello"
+
+var World : String = " World"
+
+var conc =  hello + World
+
+var conc2 = "\(hello) + \(World)"
+
+
+
+
+
+
