e207196063e07b04299f8d8283d974857911d7a3
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-09 17:03:46 +0500

    ui-fixes

--- Apps/iOS Bootcamp Loan Calculator/iOS Bootcamp Loan Calculator/ViewController.swift
+++ Apps/iOS Bootcamp Loan Calculator/iOS Bootcamp Loan Calculator/ViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var PV: UITextField!
+    @IBOutlet weak var rateperperiod: UITextField!
+    @IBOutlet weak var numberOfPeriods: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func calculate(_ sender: Any) {
+        PV.text 
+        
     }
 
 
