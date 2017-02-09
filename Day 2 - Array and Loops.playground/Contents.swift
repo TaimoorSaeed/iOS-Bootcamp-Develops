4027e1cae0cc69c8ada3091acc67cd671ac90f87
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-09 14:26:00 +0500

    array and loops

---
+++ Day 2 - Array and Loops.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var oddNumbers = [Int]()
+ 
+for index in 1...100{
+    if (index % 2 != 0){
+        oddNumbers.append(index)
+    }
+    
+}
+
+print(oddNumbers)
+
+var sums = [Int]()
+
+
+
+
