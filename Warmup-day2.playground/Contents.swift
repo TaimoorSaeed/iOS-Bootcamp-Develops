40eedd35731a941add8b517b92f035b2f36e432f
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-09 08:12:58 +0500

    warmup

---
+++ Warmup-day2.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func calcCircleArea(radius: Double) -> Double{
+    var area: Double = 3.14 * radius
+    return area
+}
+
+calcCircleArea(radius: 2)
+
+
+
