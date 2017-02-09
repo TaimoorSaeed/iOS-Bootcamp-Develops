3f1253b765e132536c69785dffb590f740c75246
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-09 19:09:23 +0500

    completed

--- Apps/iOS Bootcamp Loan Calculator/iOS Bootcamp Loan Calculator/ViewController.swift
+++ Apps/iOS Bootcamp Loan Calculator/iOS Bootcamp Loan Calculator/ViewController.swift
@@ -13,19 +13,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var PV: UITextField!
     @IBOutlet weak var rateperperiod: UITextField!
-    @IBOutlet weak var numberOfPeriods: UILabel!
+    
+    @IBOutlet weak var NumberofPeriods: UITextField!
+    @IBOutlet weak var ansLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
     
     @IBAction func calculate(_ sender: Any) {
-        PV.text 
+        let pv = Int(PV.text!)
+        let rate = Int(rateperperiod.text!)
+        let noOfPeriods = Int(NumberofPeriods.text!)
         
+    
+        let p = ((rate! * pv!) / (1 - ( 1 + rate! ) ^ (-1 * noOfPeriods!)))
+        ansLabel.text = String(p)
     }
-
 
 }
 
