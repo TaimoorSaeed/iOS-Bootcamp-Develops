093a6e54796a1e1067e82ef6542d96367c68ee32
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-09 08:02:50 +0500

    arrays

---
+++ iOS Bootcamp Arrays.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var array = [String]()
+
+var array2 : [Double] = [4.2, 5.2, 6.9, 7.1]
+
+var array3 = [1,2,3,4,5]
+
+array.append("3")
+array
+array2.append(3)
+array3.append(3)
+
+
+array2.remove(at:2)
+array2
+
+array.remove(at:0)
+
+array3.remove(at: 4)
+array3
+
+array2.removeAll()
+array2
+
+array3.removeLast()
+array3
+
+
+
+
+
+
+
+
+
+
+
